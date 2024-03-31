e0887ea90dcad70ca0e94c7e7e83be0a13c993d8
Shanvi2005 <144417466+Shanvi2005@users.noreply.github.com>
2024-03-31 21:03:51 +0530

    Create BruteForce.java

---
+++ 136. Single Number/BruteForce.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNumber(int[] arr) {
+        int n = arr.length;
+         for (int i = 0; i < n; i++) {
+            int num = arr[i]; // selected element
+            int cnt = 0;
+
+            //find the occurrence using linear search:
+            for (int j = 0; j < n; j++) {
+                if (arr[j] == num)
+                    cnt++;
+            }
+
+            // if the occurrence is 1 return ans:
+            if (cnt == 1) return num;
+        }
+        return -1;
+    }
+}
