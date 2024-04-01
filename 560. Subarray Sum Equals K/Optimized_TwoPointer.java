5b03a77fb5508407dcb6d568debc86497994e27c
Shanvi2005 <144417466+Shanvi2005@users.noreply.github.com>
2024-04-01 23:07:55 +0530

    Create Optimized_TwoPointer.java

---
+++ 560. Subarray Sum Equals K/Optimized_TwoPointer.java
@@ -0,0 +1,25 @@
+ public static int getLongestSubarray(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        int left = 0, right = 0; // 2 pointers
+        long sum = a[0];
+        int maxLen = 0;
+        while (right < n) {
+            // if sum > k, reduce the subarray from left
+            // until sum becomes less or equal to k:
+            while (left <= right && sum > k) {
+                sum -= a[left];
+                left++;
+            }
+
+            // if sum = k, update the maxLen i.e. answer:
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+
+            // Move forward thw right pointer:
+            right++;
+            if (right < n) sum += a[right];
+        }
+
+        return maxLen;
