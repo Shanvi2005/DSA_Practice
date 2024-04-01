b864ebc847ad272ebc8be93b8d1d063cf2adfef7
Shanvi2005 <144417466+Shanvi2005@users.noreply.github.com>
2024-04-01 07:07:51 +0530

    Create HashingBetterApp.java

---
+++ 560. Subarray Sum Equals K/HashingBetterApp.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int subarraySum(int[] a, int k) {
+        int n = a.length;
+       HashMap<Long,Integer> preSumMap = new HashMap<>();
+       int maxLen=0;
+       long sum =0;
+       for(int i=0;i<n;i++){
+           sum+=a[i];
+           if(sum==k){
+               maxLen=Math.max(maxLen,i+1);
+           }
+           long rem = sum-k;
+           if(preSumMap.containsKey(rem)){
+               int len = i-preSumMap.get(rem);
+               maxLen = Math.max(len,maxLen);
+           }
+           if(!preSumMap.containsKey(sum)){
+               preSumMap.put(sum,i);
+           }
+       }
+       return maxLen;
+    }
+}
