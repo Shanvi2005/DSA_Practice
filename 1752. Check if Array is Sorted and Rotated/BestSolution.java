e4840844771d17d613db683c12091b97cafadd74
Shanvi2005 <144417466+Shanvi2005@users.noreply.github.com>
2024-04-04 23:26:24 +0530

    Create BestSolution.java

---
+++ 1752. Check if Array is Sorted and Rotated/BestSolution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int c=0;
+        for(int i=0;i<nums.length-1;i++){
+            if (nums[i]>nums[i+1]){
+                c++;
+            }
+        }
+        if(nums[0]<nums[nums.length-1]){
+            c++;
+        }
+        if(c>1){
+            return false;
+        }
+        return true;
+
+    }
+}
