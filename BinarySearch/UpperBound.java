07f695b1cf23c11a203c26a1ecf20dd689da4e58
Shanvi2005 <144417466+Shanvi2005@users.noreply.github.com>
2024-05-21 14:08:21 +0530

    Create UpperBound.java

---
+++ BinarySearch/UpperBound.java
@@ -0,0 +1,23 @@
+class Solution{
+    
+    // Function to find floor of x
+    // arr: input array
+    // n is the size of array
+    static int upperBound(long arr[], int n, long x)
+    {
+        int high=n-1,low=0,pos=-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]>x){
+                pos = mid;
+                high=mid -1;
+            }
+            else {
+                low=mid+1;
+            }
+        }
+        return pos;
+            
+    }
+    
+}
